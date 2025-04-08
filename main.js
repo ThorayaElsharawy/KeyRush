4ad929bb98b4a15f5b5bbc0652318086e4493382
thoraya elsharawy <thorayaelsharawy@gmail.com>
2025-04-08 11:09:59 +0300

    feat: add countdown timer for typing test

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
+const times = document.querySelectorAll('li')
+const timerContainer = document.querySelector('.timer-container')
+let selectedTime = 5;
+const wpmValue = document.querySelector('.wpm-value')
+const AccValue = document.querySelector('.accuracy-value')
+
+times.forEach(item => {
+    item.addEventListener('click', () => {
+        times.forEach(time => time.classList.remove('active'))
+        item.classList.add('active')
+        selectedTime = item.innerHTML
+    })
+
+    document.querySelector('.time-value').innerHTML = selectedTime
+})
+
+
 const typingBtn = document.querySelector('.typing-btn');
 const container = document.querySelector('.container')
 const inputField = document.querySelector('input')
-const p = 'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet'
+const p = `The quick brown fox jumps over the lazy dog. Typing tests help improve speed and accuracy. 
+Practice daily to build muscle memory and confidence.`
 const letters = p.split('')
 const text = document.createElement('p')
 
@@ -10,21 +28,47 @@ const charElements = []
 letters.forEach(item => {
     const charachter = document.createElement('span');
     charachter.innerText = item
-   charElements.push(charachter)
+    charElements.push(charachter)
 })
 
 text.className = 'typing'
-const target = container.children[2]
+const target = container.children[3]
 text.append(...charElements)
 const userInputs = []
 
 typingBtn.onclick = () => {
-    document.querySelector('input').focus()
+    let counter = selectedTime
+    inputField.value = ''
+    userInputs.length = 0
+    charElements.forEach(item => item.className = '')
+
+    timerContainer.innerHTML = counter
+    const timer = setInterval(() => {
+        timerContainer.innerHTML = counter
+        counter--
+        if (counter < 0) {
+            clearInterval(timer)
+            timerContainer.innerHTML = 'Time is up'
+            inputField.disabled = true
+
+            const correctChars = userInputs.filter((char, index) => char === letters[index])
+
+            const wpm = (correctChars.length * 60) / (selectedTime * 5)
+            const accuracy = (correctChars.length / userInputs.length) * 100
+
+            wpmValue.innerHTML = wpm
+            AccValue.innerHTML = `${isNaN(accuracy) ? '0' : Math.round(accuracy)}%`
+            console.log(accuracy);
+            
+        }
+    }, 1000)
+
     typingBtn.style.display = 'none'
     container.insertBefore(text, target)
+    document.querySelector('input').focus()
 
-    inputField.addEventListener('input', (e) => {       
-        if(e.inputType === 'deleteContentBackward') {
+    inputField.addEventListener('input', (e) => {
+        if (e.inputType === 'deleteContentBackward') {
             userInputs.pop()
             charElements[userInputs.length].className = ''
             return
@@ -32,10 +76,13 @@ typingBtn.onclick = () => {
         userInputs.push(e.data)
 
         let currIdx = 0
-         while (currIdx < userInputs.length && currIdx < letters.length ) {   
-            const state =  letters[currIdx] === userInputs[currIdx] ? 'correct' : 'wrong'
+        while (currIdx < userInputs.length && currIdx < letters.length) {
+            const state = letters[currIdx] === userInputs[currIdx] ? 'correct' : 'wrong'
             charElements[currIdx].className = state
             currIdx++
         }
     })
+
+
 }
+
