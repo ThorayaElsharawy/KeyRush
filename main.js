e6f9d6c47f91d761feb2a2c05616e01abc0fe577
thoraya elsharawy <thorayaelsharawy@gmail.com>
2025-04-16 08:46:23 +0300

    finish app

--- main.js
+++ main.js
@@ -1,122 +1,193 @@
-const times = document.querySelectorAll('li')
-const timerContainer = document.querySelector('.timer-container')
-let selectedTime = 5;
-const wpmValue = document.querySelector('.wpm-value')
-const AccValue = document.querySelector('.accuracy-value')
+const timesList = document.querySelectorAll('#time li')
+const wordsList = document.querySelectorAll('#word li')
+const quoteList = document.querySelectorAll('#quote li')
+const timer = document.querySelector('.timer')
 const resetBtn = document.querySelector('.reset')
-
-let timerId;
-
-timerContainer.innerHTML = selectedTime
-
-times.forEach(item => {
-    item.addEventListener('click', () => {
-        times.forEach(time => time.classList.remove('active'))
-        item.classList.add('active')
-        selectedTime = item.innerHTML
-
-        timerContainer.innerHTML = selectedTime
-    })
-
-    document.querySelector('.time-value').innerHTML = selectedTime
-    
-})
-
-
-const typingBtn = document.querySelector('.typing-btn');
-const container = document.querySelector('.container')
+const StartBtn = document.querySelector('.startBtn');
+const wordsWrapper = document.querySelector('.words-wrapper')
 const inputField = document.querySelector('input')
-const p = `The quick brown fox jumps over the lazy dog. Typing tests help improve speed and accuracy. 
-Practice daily to build muscle memory and confidence.`
-const letters = p.split('')
-const text = document.createElement('p')
+const options = document.querySelectorAll('#options-list li')
+const menus = document.querySelectorAll('.option-menu')
+
 
 const charElements = []
+let userInputs = []
+let letters = []
+let selectedTime = 5;
+let selectedWord = 10;
+let selectedQuote = 'Short'
+let timerId;
+let counter = selectedTime
+let type = 'time';
 
-letters.forEach(item => {
-    const charachter = document.createElement('span');
-    charachter.innerText = item
-    charElements.push(charachter)
-})
+const resetInput = () => {
+    inputField.value = '';
+    userInputs = [];
 
-text.className = 'typing'
-const target = container.children[3]
-text.append(...charElements)
-const userInputs = []
-let counter;
+}
 
-resetBtn.onclick = () => {
-    console.log('here');
-
-    inputField.value = ''
-    userInputs.length = 0
-    charElements.forEach(item => item.className = '')
-    typingBtn.style.display = 'inline-block'
-    text.style.display = 'none'
-    AccValue.innerHTML = '0%'
-    wpmValue.innerHTML = 0;
+const reset = () => {
+    resetInput()
+    generateWords()
     clearInterval(timerId)
     inputField.disabled = false
-    counter = selectedTime
-    timerContainer.innerHTML = counter
+    inputField.focus()
 }
 
 
-typingBtn.onclick = () => {
-    counter = selectedTime
+timesList.forEach(item => {
+    item.onclick = () => {
+        timesList.forEach(time => time.classList.remove('active'))
+        item.classList.add('active')
+        selectedTime = +item.innerHTML
+        timer.innerText = selectedTime
+        counter = selectedTime;
+        reset()
+        counterFun()
+    }
+});
 
-    text.style.display = 'block'
+wordsList.forEach(word => {
+    word.onclick = () => {
+        wordsList.forEach(word => word.classList.remove('active'))
+        word.classList.add('active')
+        selectedWord = +word.innerHTML
+        reset()
+    }
+})
 
-    inputField.value = ''
-    userInputs.length = 0
-    charElements.forEach(item => item.className = '')
+quoteList.forEach(quote => {
+    quote.onclick = () => {
+        quoteList.forEach(quote => quote.classList.remove('active'))
+        quote.classList.add('active')
+        selectedQuote = quote.innerHTML
+        reset()
+    }
+})
 
-    timerContainer.innerHTML = counter
+options.forEach(option => {
+    option.onclick = () => {
+        options.forEach(option => option.classList.remove('active-type'))
+        option.classList.add('active-type');
 
-    timer()
+        const dataType = option.getAttribute('data-type')
+        type = dataType
+        menus.forEach(menu => {
+            menu.style.display = dataType === menu.id ? 'flex' : 'none'
+        })
+        reset()
 
-    typingBtn.style.display = 'none'
-    container.insertBefore(text, target)
-    document.querySelector('input').focus()
-
-    inputField.addEventListener('input', (e) => {
-        if (e.inputType === 'deleteContentBackward') {
-            userInputs.pop()
-            charElements[userInputs.length].className = ''
-            return
+        if (type === 'time') {
+            counter = selectedTime
+            timer.style.display = 'inline-block'
+            timer.innerHTML = counter
+            counterFun()
         }
-        userInputs.push(e.data)
+    }
+})
 
-        let currIdx = 0
-        while (currIdx < userInputs.length && currIdx < letters.length) {
-            const state = letters[currIdx] === userInputs[currIdx] ? 'correct' : 'wrong'
-            charElements[currIdx].className = state
-            currIdx++
+wordsWrapper.style.display = 'none'
+
+
+const generateWords = () => {
+    let p = `the quick brown fox jumps over the lazy dog. Typing tests help improve speed and accuracy. 
+    Practice daily to build muscle memory and confidence.`
+
+    const texts = {
+        default: `the quick brown fox jumps over the lazy dog. Typing tests help improve speed and accuracy. 
+                Practice daily to build muscle memory and confidence.`,
+        word: {
+            10: 'rain fell softly, calming the night with its gentle rhythm.',
+            20: 'the sun dipped below the horizon, casting golden hues across the sky as waves gently kissed the shore in perfect rhythm.',
+            30: 'as the sun set behind the mountains, the sky transformed into a canvas of vibrant colors. Birds returned to their nests, and a gentle breeze rustled the trees. The world seemed to pause for a moment, wrapped in peace, reminding everyone to slow down and appreciate life’s simple, beautiful moments.'
+        },
+        quote: {
+            Short: "less is more.",
+            Medium: "success is not final, failure is not fatal: it is the courage to continue that counts.",
+            Long: "your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma — which is living with the results of other people's thinking."
         }
+    }
+
+    if (type === 'word') {
+        p = texts.word[selectedWord] || texts.default
+    }
+    if (type === 'quote') {
+        p = texts.quote[selectedQuote] || texts.default
+    }
+    const wordsContainer = document.querySelector('.words')
+    charElements.length = 0
+    wordsContainer.innerHTML = ''
+    letters = p.split('')
+    letters.forEach(item => {
+        const charachter = document.createElement('span');
+        charachter.innerText = item
+        charElements.push(charachter)
     })
 
+    wordsContainer.append(...charElements);
 
+
+    if (type === 'word' || type === 'quote') {
+        timer.style.display = 'none'
+    }
 }
 
 
-const timer = () => {
+StartBtn.onclick = () => {
+    generateWords()
+    if (type === 'time') {
+        timer.style.display = 'inline-block'
+        timer.innerHTML = counter
+        counterFun()
+    }
+    charElements.forEach(item => item.className = '')
+    StartBtn.style.display = 'none'
+    wordsWrapper.style.display = 'block'
+    inputField.focus()
+    checkSpelling()
+}
+
+
+const counterFun = () => {
     timerId = setInterval(() => {
-        timerContainer.innerHTML = counter
+        timer.innerHTML = counter
         counter--
         if (counter < 0) {
             clearInterval(timerId)
-            timerContainer.innerHTML = 'Time is up'
+            timer.innerHTML = 'Time is up'
             inputField.disabled = true
-
-            const correctChars = userInputs.filter((char, index) => char === letters[index])
-
-            const wpm = (correctChars.length * 60) / (selectedTime * 5)
-            const accuracy = (correctChars.length / userInputs.length) * 100
-
-            wpmValue.innerHTML = wpm
-            AccValue.innerHTML = `${isNaN(accuracy) ? '0' : Math.round(accuracy)}%`
-            console.log(accuracy);
-
         }
     }, 1000)
+}
+
+const handleTyping = (e) => {
+    if (e.inputType === 'deleteContentBackward') {
+        userInputs.pop()
+        charElements[userInputs.length].className = ''
+        return
+    }
+    userInputs.push(e.data)
+    let currIdx = 0
+    while (currIdx < userInputs.length && currIdx < letters.length) {
+        const state = letters[currIdx] === userInputs[currIdx] ? 'correct' : 'wrong'
+        charElements[currIdx].className = state
+        currIdx++
+    }
+}
+
+const checkSpelling = () => {
+    inputField.removeEventListener('input', handleTyping)
+    inputField.addEventListener('input', handleTyping)
+}
+
+resetBtn.onclick = () => {
+    inputField.value = ''
+    userInputs.length = 0
+    charElements.forEach(item => item.className = '')
+    StartBtn.style.display = 'block'
+    wordsWrapper.style.display = 'none'
+    clearInterval(timerId)
+    inputField.disabled = false
+    counter = selectedTime
+    timer.innerHTML = counter
 }
